16401b0c8c666fde10f81db8979638a0198e7d4d
akane9506 <akane19950611@gmail.com>
2026-09-20 09:30:26 -0700

    add chart hover effect

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -2,11 +2,12 @@ import { useGetStockQuery } from "@/hooks/queries/useGetStockQuery";
 import StockDataPlot from "@/components/StockDataPlot";
 
 export default function Chart() {
-  const { data } = useGetStockQuery("GOOG", "Days60");
+  const { data } = useGetStockQuery("AAPL", "Days60");
+
   return (
     <div>
       <h2 className="font-medium mb-2">stock price trend</h2>
-      <div className="border border-gray-300 rounded-xl px-4 py-2 w-fit">
+      <div className="border border-gray-300 rounded-xl px-6 py-2 w-fit">
         {data && <StockDataPlot data={data} />}
       </div>
     </div>
